a9bd02ade7fb4232f419efbcdfb9ea2ab1b03b6b
5usama <dhuddiusama5@gmail.com>
2025-07-25 23:05:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ const port = process.env.PORT || 9090;
   conn.ev.on('connection.update', (update) => {
   const { connection, lastDisconnect } = update
   if (connection === 'close') {
-  if (lastDisconnect?.error?.output?.statusCode;
+  const statuscode = lastDisconnect?.error?.output?.statusCode;
 	  if (!== DisconnectReason.loggedOut) {
   connectToWA()
   }
